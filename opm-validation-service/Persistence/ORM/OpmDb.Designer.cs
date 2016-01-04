30f577f34a5bbd749d14730d27325731877d0de8
Stefan Pacinda <pacinda@seznam.cz>
2016-01-05 00:27:30 +0100

    Added EF ORM mapping for opm table.

---
+++ opm-validation-service/Persistence/ORM/OpmDb.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\Ste\Documents\_BE\repository\bohemia-energy\opm-validation-service\Persistence\ORM\OpmDb.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
--- opm-validation-service/Persistence/OpmDbRepository.cs
+++ opm-validation-service/Persistence/OpmDbRepository.cs
@@ -1,13 +1,25 @@
-﻿using opm_validation_service.Models;
+﻿using System.Globalization;
+using System.Linq;
+using opm_validation_service.Models;
+using opm_validation_service.Persistence.ORM;
 
 //TODO SP: implement
 namespace opm_validation_service.Persistence {
     //TODO SP: thread-safe!!!
     public class OpmDbRepository : IOpmRepository
     {
+        readonly BE_Opm _dbContext = new BE_Opm();
+
         public bool TryGetOpm(EanEicCode code, out Opm opmForCode)
         {
-            throw new System.NotImplementedException();
+            tbl_duplicate_opms dbOpm = _dbContext.tbl_duplicate_opms.SingleOrDefault(o => o.tdo_ean.Equals(code.Code));
+            if (dbOpm != null)
+            {
+                opmForCode = new Opm(new EanEicCode(dbOpm.tdo_ean));
+                return true;
+            }
+            opmForCode = null;
+            return false;
         }
 
         public bool TryAdd(Opm opm)
