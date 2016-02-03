b35ef5f89090e625f1c15e85f44a756ae55edff0
Stefan Pacinda <pacinda@seznam.cz>
2016-02-03 01:46:09 +0100

    Added test win-authenticated git status

---
+++ opm-validation-service/Controllers/OpmDuplicityBasicController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+
+namespace opm_validation_service.Controllers
+{
+    [Authorize]
+    public class OpmDuplicityBasicController : ApiController
+    {
+        public String Get(String id)
+        {
+            return "Current Identity: " + Thread.CurrentPrincipal.Identity.Name;
+        }
+    }
+}
