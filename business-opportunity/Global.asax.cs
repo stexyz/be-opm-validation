179afba319227c92405dbb14ef59bbf9068994c0
Stefan Pacinda <pacinda@seznam.cz>
2017-09-19 00:07:52 +0200

    Business-opportunity: added help pages.

--- business-opportunity/Global.asax.cs
+++ business-opportunity/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace business_opportunity
@@ -12,6 +13,7 @@ namespace business_opportunity
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
         }
     }
 }
